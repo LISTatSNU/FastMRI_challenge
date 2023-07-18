f6af88670b8241bddf63f2d0e33e1584673ddc7b
SEO JUN PYO <39179946+Giventicket@users.noreply.github.com>
2023-07-19 00:25:05 +0900

    Update reconstruct.py

--- reconstruct.py
+++ reconstruct.py
@@ -13,8 +13,7 @@ def parse():
     parser.add_argument('-g', '--GPU_NUM', type=int, default=0, help='GPU number to allocate')
     parser.add_argument('-b', '--batch-size', type=int, default=1, help='Batch size')
     parser.add_argument('-n', '--net_name', type=Path, default='test_varnet', help='Name of network')
-    parser.add_argument('-p', '--data_path', type=Path, default='/Data/leaderboard/', help='Directory of test data')
-    parser.add_argument('-m', '--mask', type=str, default='acc4', choices=['acc4', 'acc8'], help='type of mask | acc4 or acc8')
+    parser.add_argument('-p', '--path_data', type=Path, default='/Data/leaderboard/', help='Directory of test data')
     
     parser.add_argument('--cascade', type=int, default=1, help='Number of cascades | Should be less than 12')
     parser.add_argument('--chans', type=int, default=9, help='Number of channels for cascade U-Net')
@@ -27,8 +26,17 @@ def parse():
 
 if __name__ == '__main__':
     args = parse()
-    args.data_path = args.data_path / args.mask
     args.exp_dir = '../result' / args.net_name / 'checkpoints'
-    args.forward_dir = '../result' / args.net_name / 'reconstructions_leaderboard' / args.mask
+    
+    # acc4
+    args.data_path = args.path_data / "acc4"
+    args.forward_dir = '../result' / args.net_name / 'reconstructions_leaderboard' / "acc4"
     print(args.forward_dir)
     forward(args)
+    
+    # acc8
+    args.data_path = args.path_data / "acc8"
+    args.forward_dir = '../result' / args.net_name / 'reconstructions_leaderboard' / "acc8"
+    print(args.forward_dir)
+    forward(args)
+    
